1e29340f5f30f9705560584602576144343a25c4
shaheen37 <85019052+shaheen37@users.noreply.github.com>
2021-06-18 15:58:25 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,41 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+#include <cs50.h>
+
+int max(int arr[],int count)
+{
+    int max = arr[0];
+    for(int i=0;i<count;i++)
+    {
+        if(arr[i]>max)
+        {
+            max=arr[i];
+        }
+    }
+    return max;
+}
+
+int min(int arr[],int count)
+{
+    int min = arr[0];
+    for(int i=0;i<count;i++)
+    {
+        if(arr[i]<min)
+        {
+            min=arr[i];
+        }
+    }
+    return min;
+}
+
+float average(int arr[],int count)
+{
+    float avv = 0;
+    for(int i=0;i<count;i++)
+    {    
+    avv+=arr[i];
+    }
+
+    avv/=count;
+    return avv;
+}
